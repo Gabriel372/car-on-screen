0483e1babb84ec0cdede99fe4bf5dafc6911f648
Gabriel372 <gabriel-dobt@hotmail.com>
2024-05-21 10:25:29 -0300

    update

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -10,16 +10,16 @@ return (<footer className='bg-red-700 text-white w-full flex justify-center'>
 <nav className=" flex min-h-[4rem] items-center flex-wrap p-1">
 <p>Developed by: Gabriel Brandão</p>
 <Link href='https://www.linkedin.com/in/gabriel-brand%C3%A3o-42174223a' 
-className={` text-[1.8rem] ml-2 text-slate-100 hover:text-white w-screen350:text-[1.5rem]`}>
+className={` text-[1.8rem] ml-2 text-slate-100 hover:text-white w-screen350:text-[1.5rem] transition-all duration-200 transform hover:scale-110`}>
 <FaLinkedin />
 </Link>
 
 <Link href='https://github.com/Gabriel372' 
-className={`text-[1.8rem] ml-2 text-slate-100 hover:text-white w-screen350:text-[1.5rem]`}>
+className={`text-[1.8rem] ml-2 text-slate-100 hover:text-white w-screen350:text-[1.5rem] transition-all duration-200 transform hover:scale-110`}>
 <FaGithub />
 </Link>
 
-<Link className={` text-[1.8rem] ml-2 text-slate-100 hover:text-white w-screen350:text-[1.5rem]`}
+<Link className={` text-[1.8rem] ml-2 text-slate-100 hover:text-white w-screen350:text-[1.5rem] transition-all duration-200 transform hover:scale-110`}
  href='https://whatsa.me/5521973766911/?t=Olá, vim através site criado por você'>
 <IoLogoWhatsapp />
 </Link>
